2f94045b9968780f1b08bbddc43e878c5a84c62f
tzlil <teselil123@gmail.com>
2017-10-07 12:16:44 +0300

    first commit

---
+++ src/main/java/franktech/flirtapp/MainActivity.java
@@ -0,0 +1,26 @@
+package franktech.flirtapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button facebookLogin = (Button) findViewById(R.id.open_with_facebook);
+
+        facebookLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
