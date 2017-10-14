85b12299421e59dc2ea1e1fafb95d1f5b5287447
tzlil <teselil123@gmail.com>
2017-10-14 19:19:25 +0300

    four commit

--- src/main/java/franktech/flirtapp/view/StripBar.java
+++ src/main/java/franktech/flirtapp/view/StripBar.java
@@ -1,13 +1,16 @@
 package franktech.flirtapp.view;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
 import franktech.flirtapp.R;
+import franktech.flirtapp.activity.MatchActivity;
 
 /**
  * Created by tzlil on 07/10/17.
@@ -30,8 +33,17 @@ public class StripBar extends RelativeLayout {
         initView(context);
     }
 
-    private void initView(Context context) {
+    private void initView(final Context context) {
         LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        inflater.inflate(R.layout.strip_bar, this);
+        View view = inflater.inflate(R.layout.strip_bar, this);
+        ImageButton interestedIcon= (ImageButton) view.findViewById(R.id.interested_icon);
+        interestedIcon.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, MatchActivity.class);
+                context.startActivity(intent);
+            }
+        });
+
     }
 }
